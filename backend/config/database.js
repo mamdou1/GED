26a99345d77003abb4fa2e8e75362bf394ad13cd
mamdou1 <mamadoudiouma826@gmail.com>
2026-04-02 16:38:57 +0000

    c

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "docdoc",
+  process.env.DB_NAME || "document",
   process.env.DB_USER || "root",
   process.env.DB_PASSWORD || "root",
   {
     host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 3307,
     dialect: "mysql",
     logging: false,
   },
